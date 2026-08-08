7284e039dca46a512e25288c9d644ce8f21aeea1
Glenn Thompson <glenn@paren.works>
2026-08-08 18:27:27 +0300

    Bump version to 0.8.0

--- clatter.el
+++ clatter.el
@@ -4,7 +4,7 @@
 ;; Author: Glenn Thompson <glenn@paren.works>
 ;; SPDX-License-Identifier: MIT
 ;; URL: https://github.com/parenworks/clatter.el
-;; Version: 0.7.0
+;; Version: 0.8.0
 ;; Package-Requires: ((emacs "30.1"))
 ;; Keywords: comm, irc
 
